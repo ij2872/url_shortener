7a542ecbc7a26a6ad2c5fb70e83b6fd876ab8281
Ivan <ijaimes2872@gmail.com>
2017-10-11 23:37:06 -0500

    Add Test cases added for all methods in library.js

--- test/api.test.js
+++ test/api.test.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert;
 var request = require('supertest');
 
-const {generate} = require('../functions/library');
+const {charToInt, getRegexStrArray, encode, generate} = require('../functions/library');
 var app = require('../app');
 
 
@@ -26,8 +26,46 @@ describe('Api', function(){
 
 
 describe("URL-Generator", function(){
-    
-    it("generate('aaa') Expected 30", function(){
+    it("charToInt_0_0", function(){
+        let result = charToInt(0);
+        assert.equal(result, "0");
+    });
+
+    it("charToInt_d_13", function(){
+        let result = charToInt('d');
+        assert.equal(result, "13");
+    });
+
+    it("getRegexStrArray_te.st_test", function(){
+        let result = "";
+        let resultArr = getRegexStrArray("te.st");
+        for(let i in resultArr){
+            result += resultArr[i];
+        }
+        assert.equal(result, "test");
+    });
+
+    it("encode'youtube'_172", function(){
+        let result = encode("youtube");
+        assert.equal(result, '172');
+    });
+
+    it("encode'aaa'_30", function(){
+        let result = encode("aaa");
+        assert.equal(result, '30');
+    });
+
+    it("generate_''_0", function(){
+        let result = generate("");
+        assert.equal(result, '0');
+    });
+
+    it("generate_'youtube'_172", function(){
+        let result = generate("youtube");
+        assert.equal(result, '172');
+    });
+
+    it("generate_'aaa'_30", function(){
         let result = generate("aaa");
         assert.equal(result, '30');
     });
