0482ce45e6127a67f5cf9bd915530dcb17edf6df
Ivan <ijaimes2872@gmail.com>
2017-10-12 03:21:35 -0500

    Added Ability to uploud and read from database

--- routes/api.js
+++ routes/api.js
@@ -24,15 +24,34 @@ router.get('/url', function(req, res){
 });
 
 router.get('/url/:str', function(req, res){
-  let str = req.body.str;
+  let str = req.params.str;
+
   let strId = generate(str);
-  db.collection('url').save({url: str, shortenId: strId});
+  // let dbArray = db.collection('url').find({shortenId: {$eq: strId}});
+
+  db.collection('url').save({_id: strId, url: str });
+});
+
+// Returns full url from
+router.get('/find/:str', function(req, res){
+  let str = req.params.str.toString();
+  let result = "";
+  db.collection('url').findOne({"_id": str})
+      .then(function(data){
+        result = data.url;
+        console.log(result);
+      });
+
+  res.send(result);
 });
 
 // returns {url} JSON from a post request
 router.post('/url', function(req, res){
-  const result = generate(req.body.url);
-  res.send(result);
+  let str = req.body.url;
+  let strId = generate(str);
+
+  db.collection('url').save({_id: strId, url: str });
+  res.send(202);
 });
 
 module.exports = router;
