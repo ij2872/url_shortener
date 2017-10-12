932dc985649a2ae573c64a7e874ed7a273792342
Ivan <ijaimes2872@gmail.com>
2017-10-11 19:42:00 -0500

    Init commit

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('API Connections');
+});
+
+router.get(){
+
+};
+
+module.exports = router;
